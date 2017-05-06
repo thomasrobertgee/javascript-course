99cf3a6f963134826cb79719ec4099dc5c78a736
Thomas Gee <thomasrobertgee@gmail.com>
2017-05-06 19:17:16 +1000

    variables, operators

---
+++ 2-JS-Basics/script.js
@@ -0,0 +1,64 @@
+/*
+
+// Lecture variables
+var name = 'John'
+console.log(name)
+
+var lastName = 'Smith'
+console.log(lastName)
+
+var age = 26
+
+console.log(age)
+
+console.log(name + ' ' + lastName + ' is ' + age + ' old.')
+
+var fullAge = true
+console.log(fullAge)
+
+// Lecture variables 2
+var name = 'Tom '
+var age = 22
+
+console.log(name + age)
+console.log(age + age)
+console.log(age + '' + age)
+console.log(age + ' ' + age * age)
+
+var job, isMarried
+console.log(job)
+
+job = 'teacher'
+isMarried = false
+
+console.log(name + 'is a ' + job, '. Is he married? ' + isMarried)
+
+var lastName = prompt('What is the last name?')
+
+console.log(lastName)
+
+alert(lastName)
+
+*/
+
+// Lecture: operators
+
+// var now = 2017
+// var birthYear = now - 22
+//
+// birthYear = now - 22 * 2
+//
+//
+// console.log(birthYear)
+//
+// var ageTom = 23
+// var ageMark = 28
+//
+// ageJohn = (3 + 5) * 4 - 6
+// ageJohn++ // +1
+// console.log(ageJohn) // 27
+//
+// ageJohn *= 2
+// console.log(ageJohn)
+
+// Lecture: if/else statements
