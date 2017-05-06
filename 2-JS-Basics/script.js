c9baa4d04f6c98d29c9cb51f68506b8e92439c66
Thomas Gee <thomasrobertgee@gmail.com>
2017-05-06 23:33:18 +1000

    if/else statements, boolean logic, code challenge

--- 2-JS-Basics/script.js
+++ 2-JS-Basics/script.js
@@ -62,3 +62,88 @@ alert(lastName)
 // console.log(ageJohn)
 
 // Lecture: if/else statements
+
+// var name = 'John'
+// var age = 26
+// var isMarried = 'yes'
+//
+// if (isMarried === 'yes') {
+//   console.log(name + ' is married!')
+// } else {
+//   console.log(name + ' will hopefully marry soon..')
+// }
+//
+// isMarried = true
+//
+// if(isMarried) {
+//   console.log('YES!')
+// } else {
+//   console.log('NO!')
+// }
+//
+// if (23 == "23") {
+//   console.log('Something to print...')
+// } // true
+//
+// if (23 === "23") {
+//   console.log('Something to 2...')
+// } // false
+
+// Lecture: boolean logic and switch
+
+// var age = 20
+//
+//
+// if (age > 19 ) {
+//   console.log('John is a teenager')
+// } else if (age >= 20 && age < 30 ) {
+//   console.log('John is a young man')
+// } else {
+//   console.log('John is a man')
+// }
+//
+// var job = 'teacher'
+//
+// job = prompt('What does John do?')
+//
+// switch (job) {
+//   case 'teacher':
+//     console.log('John teaches kids.')
+//     break
+//   case 'driver':
+//     console.log('John drives a cab in Lisbon.')
+//     break
+//   case 'cop':
+//     console.log('John helps fight crime.')
+//     break
+//   default:
+//     console.log('John does something else.')
+// }
+
+var johnHeight = 170
+var tomHeight = 175
+var baxHeight = 183
+
+var johnAge = 70
+var tomAge = 25
+var baxAge = 23
+
+var multiplier = 90
+
+var johnScore = johnHeight + (johnAge * multiplier)
+var tomScore = tomHeight + (tomAge * multiplier)
+var baxScore = baxHeight + (baxAge * multiplier)
+
+console.log('Johns score = ' + johnScore)
+console.log('Toms score = ' + tomScore)
+console.log('Baxters score = ' + baxScore)
+
+if (tomScore > johnScore && tomScore > baxScore ) {
+  console.log('Tom wins with ' + tomScore + ' points.')
+} else if (johnScore > tomScore && johnScore >  baxScore) {
+  console.log('John wins with ' + johnScore + ' points.')
+} else if (baxScore > tomScore && baxScore > johnScore) {
+  console.log('Baxter wins with ' + baxScore + ' points.')
+} else {
+  console.log('It\'s a draw!')
+}
