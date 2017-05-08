bf100d84bd910e58069f71df4c80c94a3234042f
Thomas Gee <thomasrobertgee@gmail.com>
2017-05-08 15:11:53 +1000

    halfway through dice game

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -8,18 +8,49 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer, dice
+
+var scores, roundScore, activePlayer
 
 scores = [0, 0]
 roundScore = 0
-activePlayer = 1
-
-dice = Math.floor(Math.random() * 6) + 1
-
-document.querySelector('#current-' + activePlayer).textContent = dice // changes text content
-// document.querySelector('#current-' + activePlayer).innerHtml = '<em>' + dice + '</em>'; adds html
-
-var x = document.querySelector('#score-0').textContent
-console.log(x) // reads DOM
+activePlayer = 0
 
 document.querySelector('.dice').style.display = 'none' // changes css
+
+document.getElementById('score-0').textContent = '0'
+document.getElementById('score-1').textContent = '0'
+document.getElementById('current-0').textContent = '0'
+document.getElementById('current-1').textContent = '0'
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  // 1. random number
+  var dice = Math.floor(Math.random() * 6) + 1
+
+  // 2. display result
+  var diceDOM = document.querySelector('.dice')
+  diceDOM.style.display = 'block'
+  diceDOM.src = 'dice-' + dice + '.png'
+
+  // 3. update the round score IF the rolled number was NOT a 1
+  if (dice !== 1) {
+    // add score
+    roundScore += dice // roundScore = roundScore + dice
+    document.querySelector('#current-' + activePlayer).textContent = roundScore
+  } else {
+    // next player
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0 // cleaner way of writing IF statement
+    roundScore = 0
+
+    document.getElementById('current-0').textContent = '0'
+    document.getElementById('current-1').textContent = '0'
+
+    document.querySelector('.player-0-panel').classList.toggle('active')
+    document.querySelector('.player-1-panel').classList.toggle('active')
+    // document.querySelector('.player-0-panel').classList.remove('active')
+    // document.querySelector('.player-1-panel').classList.add('active')
+
+  }
+})
+
+// document.querySelector('#current-' + activePlayer).textContent = dice // changes text content
+// document.querySelector('#current-' + activePlayer).innerHtml = '<em>' + dice + '</em>'; adds html
