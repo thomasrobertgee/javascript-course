6d7116016173f8caf8b79d31d15768acdc134517
Thomas Gee <thomasrobertgee@gmail.com>
2017-05-09 15:41:21 +1000

    passing functions as arguments, functions returning functions, IIFE, closures

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -88,3 +88,142 @@ function change(a, b) {
 change(age, obj)
 console.log(age, obj.city)
 */
+
+// Lectue: Passing functions as arguments
+/*
+var years = [1990, 1965, 1937, 2005, 1998, 1995]
+
+function arrayCalc(arr, fn) {
+  var arrRes = []
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]))
+  }
+  return arrRes
+}
+
+function calculateAge(el) {
+  return 2017 - el
+}
+
+function isFullAge(el) {
+  return el >= 18
+}
+
+function maxHeartRate(el) {
+  if (el >= 18 && el <= 81) {
+    return Math.round(206.9 - (0.67 * el))
+  } else {
+    return -1
+  }
+}
+
+var ages = arrayCalc(years, calculateAge)
+var fullAges = arrayCalc(ages, isFullAge)
+var rates = arrayCalc(ages, maxHeartRate)
+
+console.log(ages)
+console.log(fullAges)
+console.log(rates)
+*/
+
+// Lectures: Functions returning functions
+/*
+function interviewQuestion(job) {
+  if (job === 'designer') {
+    return function(name) {
+      console.log(name, 'can you please explain UX?')
+    }
+  } else if (job === 'teacher') {
+    return function(name) {
+      console.log('What do you teach', name)
+    }
+  } else {
+    return function(name) {
+      console.log('Hello', name)
+    }
+  }
+}
+
+var teacherQuestion = interviewQuestion('teacher')
+var designerQuestion = interviewQuestion('designer')
+
+teacherQuestion('John')
+designerQuestion('John')
+
+interviewQuestion('teacher')('Alex')
+*/
+
+// Lecture: IIFE
+/*
+function game() {
+  var score = Math.random() * 10
+  console.log(score >= 5)
+}
+game()
+*/
+/*
+(function () {
+  var score = Math.random() * 10
+  console.log(score >= 5)
+})()
+
+// console.log(score)
+
+(function (goodLuck) {
+  var score = Math.random() * 10
+  console.log(score >= 5 - goodLuck)
+})(5)
+*/
+
+// Lecture: Closures
+/*
+function retirement(retirementAge) {
+  var a = ' years left until retirement'
+  return function(yearOfBirth) {
+    var age = 2017 - yearOfBirth
+    console.log((retirementAge - age) + a)
+  }
+}
+
+var retirementUS = retirement(66)
+var retirementGermany = retirement(65)
+var retirementIceland = retirement(67)
+
+// retirement(66)(1990)
+
+retirementUS(1990)
+retirementGermany(1990)
+retirementIceland(1990)
+*/
+/*
+function interviewQuestion(job) {
+  if (job === 'designer') {
+    return function(name) {
+      console.log(name, 'can you please explain UX?')
+    }
+  } else if (job === 'teacher') {
+    return function(name) {
+      console.log('What do you teach', name)
+    }
+  } else {
+    return function(name) {
+      console.log('Hello', name)
+    }
+  }
+}
+*/
+/*
+function interviewQuestion(job) {
+  return function(name) {
+    if (job === 'designer') {
+      console.log(name, 'can you please explain UX?')
+    } else if (job === 'teacher') {
+      console.log('What do you teach', name)
+    } else {
+      console.log('Hello', name)
+    }
+  }
+}
+
+interviewQuestion('teacher')('John')
+*/
