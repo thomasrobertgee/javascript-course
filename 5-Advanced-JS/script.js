272aabf46f5e12e2bc010c7ed969f017efda9ead
Thomas Gee <thomasrobertgee@gmail.com>
2017-05-09 13:22:10 +1000

    inheritance and prototype chain, function constructors, object.create, primitives vs objects

---
+++ 5-Advanced-JS/script.js
@@ -0,0 +1,90 @@
+// Function constructor
+
+/*
+var john = {
+  name: 'John',
+  yearOfBirth: 1990,
+  job: 'teacher'
+}
+
+var Person = function(name, yearOfBirth, job) {
+  this.name = name
+  this.yearOfBirth = yearOfBirth
+  this.job = job
+}
+
+Person.prototype.calculateAge = function() {
+  console.log(2016 - this.yearOfBirth)
+}
+
+Person.prototype.lastName = 'Smith'
+
+var john = new Person('John', 1990, 'teacher')
+var jane = new Person('Jane', 1969, 'designer')
+var mark = new Person('Mark', 1948, 'retired')
+
+john.calculateAge()
+jane.calculateAge()
+mark.calculateAge()
+
+console.log(john.lastName)
+console.log(jane.lastName)
+console.log(mark.lastName)
+*/
+
+// Object.create
+/*
+var personProto = {
+  calculateAge: function() {
+    console.log(2016 - this.yearOfBirth)
+  }
+}
+
+var john = Object.create(personProto)
+john.name = 'John'
+john.yearOfBirth = 1990
+john.job = 'teacher'
+
+var jane = Object.create(personProto,
+  {
+    name: { value: 'Jane'}  ,
+    yearOfBirth: { value: 1969 },
+    job: { value: 'designer'}
+  }
+)
+*/
+
+// Primitives vs Objects
+
+// primitives
+/*
+var a = 23
+var b = a
+a = 46
+console.log(a, b)
+
+// objects
+var obj1 = {
+  name: 'John',
+  age: 26
+}
+var obj2 = obj1
+obj1.age = 30
+console.log(obj1.age)
+console.log(obj2.age)
+
+// functions
+var age = 27
+var obj = {
+  name: 'Jonas',
+  city: 'Lisbon'
+}
+
+function change(a, b) {
+  a = 30
+  b.city = 'San Francisco'
+}
+
+change(age, obj)
+console.log(age, obj.city)
+*/
