5f4442af7c8122dd704f9f2259a019029809113b
Thomas Gee <thomasrobertgee@gmail.com>
2017-05-09 17:17:29 +1000

    coding challenge 4 completed

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -89,7 +89,7 @@ change(age, obj)
 console.log(age, obj.city)
 */
 
-// Lectue: Passing functions as arguments
+// Lecture: Passing functions as arguments
 /*
 var years = [1990, 1965, 1937, 2005, 1998, 1995]
 
@@ -227,3 +227,150 @@ function interviewQuestion(job) {
 
 interviewQuestion('teacher')('John')
 */
+
+// Lecture: Bind, Call & Apply
+/*
+var john = {
+  name: 'John',
+  age: 26,
+  job: 'teacher',
+  presentation: function(style, timeOfDay) {
+    if (style === 'formal') {
+      console.log('Good', timeOfDay, 'ladies & gentlemen. I\'m', this.name, 'I\'m a', this.job, 'and I\'m', this.age + ' years old.')
+    } else if (style === 'friendly') {
+      console.log('Hey, what\'s up! I\'m', this.name, 'I\'m a', this.job, 'and I\'m ' + this.age + ' years old. Have a nice', timeOfDay)
+    }
+  }
+}
+
+var emily = {
+  name: 'Emily',
+  age: 35,
+  job: 'designer'
+}
+
+john.presentation('formal', 'morning')
+
+john.presentation.call(emily, 'friendly', 'afternoon') // borrow method from john to use with emily
+
+var johnFriendly = john.presentation.bind(john, 'friendly')
+
+johnFriendly('morning')
+johnFriendly('night')
+
+var emilyFormal = john.presentation.bind(emily, 'formal')
+emilyFormal('afternoon')
+*/
+/*
+var years = [1990, 1965, 1937, 2005, 1998, 1995]
+
+function arrayCalc(arr, fn) {
+  var arrRes = []
+  for (var i = 0; i < arr.length; i++) {
+    arrRes.push(fn(arr[i]))
+  }
+  return arrRes
+}
+
+function calculateAge(el) {
+  return 2017 - el
+}
+
+function isFullAge(limit, el) {
+  return el >= limit
+}
+
+var ages = arrayCalc(years, calculateAge)
+var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20))
+var fullAustralia = arrayCalc(ages, isFullAge.bind(this, 18))
+console.log(ages)
+console.log(fullJapan)
+console.log(fullAustralia)
+*/
+
+// Coding challenge 4
+
+(function() {
+  function Question(question, answers, correct) {
+    this.question = question
+    this.answers = answers
+    this.correct = correct
+  }
+
+  Question.prototype.displayQuestion = function() {
+    console.log(this.question)
+
+    for (var i = 0; i < this.answers.length; i++) {
+      console.log(i + ': ' + this.answers[i])
+    }
+  }
+
+  Question.prototype.checkAnswer = function(ans, callback) {
+    var sc
+
+    if (ans === this.correct) {
+      console.log('Correct answer!')
+      sc = callback(true)
+    } else {
+      console.log('Wrong answer buddy. Try again.')
+
+      sc = callback(false)
+    }
+
+    this.displayScore(sc)
+  }
+
+  Question.prototype.displayScore = function(score) {
+    console.log('Your current scrore is: ' + score)
+    console.log('----------------------------------')
+  }
+
+  var q1 = new Question(
+    'Is Javascript the coolest programming language in the world?',
+    ['Yes', 'No'],
+    0
+  )
+
+  var q2 = new Question(
+    'What is the name of this course\'s instructor?',
+    ['John', 'Jonas', 'Jamie'],
+    1
+  )
+
+  var q3 = new Question(
+    'What best describes coding?',
+    ['Boring', 'Hard', 'Fun', 'Tedious'],
+    2
+  )
+
+  var questions = [q1, q2, q3]
+
+  function score() {
+    var sc = 0
+    return function(correct) {
+      if (correct) {
+        sc++
+      }
+      return sc
+    }
+  }
+
+  var keepScore = score()
+
+  function nextQuestion() {
+
+    var n = Math.floor(Math.random() * questions.length)
+
+    questions[n].displayQuestion()
+
+    var answer = prompt('Please select the correct answer')
+
+    if(answer !== 'exit') {
+      questions[n].checkAnswer(parseInt(answer), keepScore)
+
+      nextQuestion()
+    }
+  }
+
+  nextQuestion()
+})()
