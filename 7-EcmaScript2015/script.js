d686bef49e41be11372dcf207d3f6b31a13a66e9
Thomas Gee <thomasrobertgee@gmail.com>
2017-05-12 20:59:44 +1000

    maps, classes, subclasses, coding challenge 5, course complete

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -397,3 +397,245 @@ console.log(john)
 var emily = new SmithPerson('Emily', 1983, 'Diaz', 'Spanish')
 console.log(emily)
 */
+
+///////////////////////////////////////////////////////
+
+// Lecture: Maps
+/*
+const question = new Map()
+question.set('question', 'What is the name of my dog?')
+question.set(1, 'Pebbles')
+question.set(2, 'Broseph')
+question.set(3, 'SuperDoge')
+question.set(4, 'AlphaDoge')
+question.set('correct', 1)
+question.set(true, 'Correct answer!')
+question.set(false, 'Wrong answer bro, try again!')
+
+// console.log(question.get('question'))
+// console.log(question.size)
+
+if (question.has(4)) {
+  // question.delete(4)
+  // console.log('Answer 4 is here')
+}
+
+// question.clear()
+
+// question.forEach((value, key) => console.log(`This is ${key}, and it's set to ${value}.`))
+
+for (let [key, value] of question.entries()) {
+  if (typeof(key) === 'number') {
+    console.log(`Answer ${key}: ${value}`)
+  }
+}
+
+const ans = parseInt(prompt('Write the correct answer (1, 2, 3, or 4)'))
+console.log(question.get(ans === question.get('correct')))
+*/
+
+///////////////////////////////////////////////////////
+
+// Lecture: Classes
+/*
+// ES5
+var Person5 = function(name, yearOfBirth, job) {
+  this.name = name
+  this.yearOfBirth = yearOfBirth
+  this.job = job
+}
+
+Person5.prototype.calculateAge = function() {
+  var age = new Date().getFullYear - this.yearOfBirth
+  console.log(age)
+}
+
+var john5 = new Person5('John', 1990, 'teacher')
+
+// ES6
+class Person6 {
+  constructor (name, yearOfBirth, job) {
+    this.name = name
+    this.yearOfBirth = yearOfBirth
+    this.job = job
+  }
+
+  calculateAge() {
+    const age = new Date().getFullYear - this.yearOfBirth
+    console.log(age)
+  }
+
+  static greeting() {
+    console.log('Hey there')
+  }
+
+}
+
+const john6 = new Person6('John', 1990, 'teacher')
+
+Person6.greeting()
+*/
+
+///////////////////////////////////////////////////////
+
+// Lecture: Classes with Subclasses
+/*
+// ES5
+var Person5 = function(name, yearOfBirth, job) {
+  this.name = name
+  this.yearOfBirth = yearOfBirth
+  this.job = job
+}
+
+Person5.prototype.calculateAge = function() {
+  var age = new Date().getFullYear() - this.yearOfBirth
+  console.log(age)
+}
+
+var Athlete5 = function(name, yearOfBirth, job, olympicGames, medals) {
+  Person5.call(this, name, yearOfBirth, job)
+  this.olympicGames = olympicGames
+  this.medals = medals
+}
+
+Athlete5.prototype = Object.create(Person5.prototype)
+
+Athlete5.prototype.wonMedal = function() {
+  this.medals++
+  console.log(this.medals)
+}
+
+var johnAthlete5 = new Athlete5('John', 1990, 'swimmer', 3, 10)
+
+johnAthlete5.calculateAge()
+johnAthlete5.wonMedal()
+*/
+
+// ES6
+/*
+class Person6 {
+  constructor(name, yearOfBirth, job) {
+    this.name = name
+    this.yearOfBirth = yearOfBirth
+    this.job = job
+  }
+
+  calculateAge() {
+    const age = new Date().getFullYear() - this.yearOfBirth
+    console.log(age)
+  }
+
+}
+
+class Athlete6 extends Person6 {
+  constructor(name, yearOfBirth, job, olympicGames, medals) {
+    super(name, yearOfBirth, job)
+    this.olympicGames = olympicGames
+    this.medals = medals
+  }
+
+  wonMedal() {
+    this.medals++
+    console.log(this.medals)
+  }
+}
+
+const johnAthlete6 = new Athlete6('John', 1990, 'swimmer', 3, 10)
+
+johnAthlete6.wonMedal()
+johnAthlete6.calculateAge()
+*/
+
+///////////////////////////////////////////////////////
+
+// Coding Challenge 5
+
+class Element {
+  constructor(name, buildYear) {
+    this.name = name
+    this.buildYear = buildYear
+  }
+}
+
+class Park extends Element {
+  constructor(name, buildYear, area, numTrees) {
+    super(name, buildYear)
+    this.area = area // km2
+    this.numTrees = numTrees
+  }
+  treeDensity() {
+    const density = this.numTrees / this.area
+    console.log(`${this.name} has a tree density of ${density} trees per square km.`)
+  }
+}
+
+class Street extends Element {
+  constructor(name, buildYear, length, size = 3) {
+    super(name, buildYear)
+    this.length = length
+    this.size = size
+  }
+
+  classifyStreet () {
+    const classification = new Map()
+    classification.set(1, 'tiny')
+    classification.set(2, 'small')
+    classification.set(3, 'normal')
+    classification.set(4, 'big')
+    classification.set(5, 'huge')
+    console.log(`${this.name}, built in ${this.buildYear}, is a ${classification.get(this.size)} street.`)
+  }
+}
+
+const allParks = [
+  new Park('Green Park', 1987, 0.2, 215),
+  new Park('National Park', 1894, 2.9, 3541),
+  new Park('Oak Park', 1953, 0.4, 949)
+]
+
+const allStreets = [
+  new Street('Ocean Avenue', 1999, 1.1, 4),
+  new Street('Evergreen Street', 2008, 2.7, 2),
+  new Street('4th Street', 2015, 0.8, 3),
+  new Street('Sunset Boulevard', 1982, 2.5, 5)
+]
+
+function calc(arr) {
+
+  const sum = arr.reduce((prev, cur, index) => prev + cur, 0)
+
+  return [sum, sum / arr.length]
+
+}
+
+function reportParks(p) {
+  console.log('----- PARKS REPORT -----')
+
+  // Density
+  p.forEach(el => el.treeDensity())
+
+  // Average Age
+  const ages = p.map(el => new Date().getFullYear() - el.buildYear)
+  const [totalAge, avgAge] = calc(ages)
+  console.log(`Our ${p.length} parks have an average of ${avgAge} years.` )
+
+  // Which park has more than 1000 trees
+  const i = p.map(el => el.numTrees).findIndex(el => el >= 1000)
+  console.log(`${p[i].name} has more than 1000 trees.`)
+
+}
+
+function reportStreets(s) {
+  console.log('----- STREETS REPORT -----')
+
+  // Total and averge langth of the town's streets
+  const [totalLength, avgLength] = calc(s.map(el => el.length))
+  console.log(`Our ${s.length} streets have a total length of ${totalLength}km, with an avergae of ${avgLength}km.`)
+
+  // Classify size
+  s.forEach(el => el.classifyStreet())
+
+}
+
+reportParks(allParks)
+reportStreets(allStreets)
